d639fa09040059e4c015fcc08de2e53821a4d086
Roy <royrao2333@gmail.com>
2026-05-20 16:19:26 +0800

    ✨ tailwindcss

--- README.md
+++ README.md
@@ -1,7 +1,78 @@
-# Tauri + React + Typescript
+<div align="center"><img src = "https://github.com/RoyRao2333/template-tauri-vite-react-ts-tailwind/assets/31413093/91cdcd1b-2387-4c01-9710-9b2f44c10329" height="100px" alt="Tauri"/><img src = "https://user-images.githubusercontent.com/31413093/197097625-5b3bd3cf-2bd6-4a3a-8059-a1fe9f28100b.svg" height="100px" alt="Vite"/></div>
 
-This template should help get you started developing with Tauri, React and Typescript in Vite.
+<h2 align="center">template-tauri-vite-react-ts-tailwind</h2>
 
-## Recommended IDE Setup
+<div align="center">
+<a href="https://reactjs.org/"><image src="https://img.shields.io/static/v1?label=React&message=^19&style=for-the-badge&labelColor=FFFFFF&logo=react&color=61DAFB"/></a> <a href="https://www.typescriptlang.org/"><image src="https://img.shields.io/static/v1?label=TypeScript&message=^5&style=for-the-badge&labelColor=FFFFFF&logo=typescript&color=3178C6"/></a>
+</div>
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+<div align="center">
+<a href="https://cn.vitejs.dev/"><image src="https://img.shields.io/static/v1?label=Vite&message=^7&style=for-the-badge&labelColor=FFFFFF&logo=vite&color=646CFF"/></a> <a href="https://tailwindcss.com/"><image src="https://img.shields.io/static/v1?label=Tailwind%20CSS&message=^4&style=for-the-badge&labelColor=FFFFFF&logo=tailwindcss&color=06B6D4"/></a> <a href="https://tauri.app/"><image src="https://img.shields.io/static/v1?label=Tauri&message=^2&style=for-the-badge&labelColor=FFFFFF&logo=tauri&color=FFC131"/></a>
+</div>
+
+## Introduction
+
+A starter [Tauri](https://v2.tauri.app/) template, but with pnpm monorepo and some recommended configurations:
+
+- Vite
+- React
+- TypeScript
+- Tailwind CSS
+- Oxc
+
+> **Note**
+> 
+> This code repository may occasionally update its dependency versions. If the dependency versions in the latest code do not meet your expectations, please go to [Tags](https://github.com/RoyRao2333/template-tauri-vite-react-ts-tailwind/tags) section to download a previous version of this template. I will display the dependencies used in each Tag, please choose the one you need.
+> 
+> 这个代码仓库可能会不定期更新其依赖包的版本。如果最新代码中依赖的版本不符合你的期望，请移步[Tags](https://github.com/RoyRao2333/template-tauri-vite-react-ts-tailwind/tags)下载之前的模板。每个Tag中使用的依赖都会展示在Tag详情中，请选择你需要的版本。
+
+## Install
+
+> This project uses [pnpm](https://pnpm.io/) as its package manager. Go check it out if you don't have it locally installed. If you use other package managers like *Yarn* or *npm*, you may need to change some scripts in `tauri.conf.json` and `package.json`.
+
+
+> [!TIP]
+> 
+> The fastest way to use this template is to click the “Use this template” button on the top right of this repository. It will help you create a new repository quickly, and you can make any modifications to your own repository. If you still want to download this template separately, please continue reading.
+> 
+> 使用该模板的最快方式，就是点击本仓库右上角的“使用该模板”按扭。这帮助你使用该模板迅速创建一个新的仓库，然后你可以对自己的仓库进行任何修改。如果你仍然希望单独下载此模板，可继续往下读。
+
+Then you need a copy of this repository. You can [download](https://github.com/RoyRao2333/template-tauri-vite-react-ts-tailwind/archive/refs/heads/main.zip) a copy as zip but [tiged](https://github.com/tiged/tiged) is recommended.
+
+After you installed tiged, please excute the following command:
+
+```sh
+$ cd path-to-save-your-project
+$ tiged royrao2333/template-tauri-vite-react-ts-tailwind your-project-name
+```
+
+After getting a copy of this repository, you can use your package manager to install dependecies:
+
+```sh
+$ cd path-to-your-project
+$ pnpm install
+```
+
+## Usage
+
+Let's run!
+
+```sh
+$ pnpm tauri dev
+```
+
+> We've already implemented some recommended configurations in ```.eslintrc.js```, ```.eslintignore```, ```.prettierrc.json5``` and ```.prettierignore```. Feel free to edit them if you have your own preferences.
+
+## Related Efforts
+
+- [Vite](https://github.com/vitejs/vite)
+- [Tauri](https://github.com/tauri-apps/tauri)
+- [Tailwind CSS](https://github.com/tailwindlabs/tailwindcss)
+
+## Contributing
+
+Feel free to dive in! [Open an issue](https://github.com/RoyRao2333/template-tauri-vite-react-ts-tailwind/issues/new) or submit PRs.
+
+## License
+
+[MIT](LICENSE) © Roy Rao
--- apps/tauri-app/src/App.tsx
+++ apps/tauri-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { invoke } from '@tauri-apps/api/core';
 import { useState } from 'react';
-import './App.css';
 import reactLogo from './assets/react.svg';
+import './index.css';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('');
@@ -12,37 +12,71 @@ function App() {
   }
 
   return (
-    <main className='container'>
-      <h1>Welcome to Tauri + React</h1>
+    <main className='m-0 flex min-h-screen items-center justify-center bg-[#f6f6f6] text-center font-[Inter,Avenir,Helvetica,Arial,sans-serif] text-base leading-6 font-normal text-[#0f0f0f] dark:bg-[#2f2f2f] dark:text-[#f6f6f6]'>
+      <div className='flex w-full max-w-4xl flex-col items-center gap-8 px-6 py-10'>
+        <h1 className='text-center'>Welcome to Tauri + React</h1>
 
-      <div className='row'>
-        <a href='https://vite.dev' target='_blank'>
-          <img src='/vite.svg' className='logo vite' alt='Vite logo' />
-        </a>
-        <a href='https://tauri.app' target='_blank'>
-          <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        <div className='flex justify-center'>
+          <a
+            href='https://vite.dev'
+            target='_blank'
+            className='font-medium text-[#646cff] no-underline hover:text-[#535bf2] dark:hover:text-[#24c8db]'
+          >
+            <img
+              src='/vite.svg'
+              className='h-[6em] p-[1.5em] transition-[filter] duration-[750ms] will-change-[filter] hover:drop-shadow-[0_0_2em_#747bff]'
+              alt='Vite logo'
+            />
+          </a>
+          <a
+            href='https://tauri.app'
+            target='_blank'
+            className='font-medium text-[#646cff] no-underline hover:text-[#535bf2] dark:hover:text-[#24c8db]'
+          >
+            <img
+              src='/tauri.svg'
+              className='h-[6em] p-[1.5em] transition-[filter] duration-[750ms] will-change-[filter] hover:drop-shadow-[0_0_2em_#24c8db]'
+              alt='Tauri logo'
+            />
+          </a>
+          <a
+            href='https://react.dev'
+            target='_blank'
+            className='font-medium text-[#646cff] no-underline hover:text-[#535bf2] dark:hover:text-[#24c8db]'
+          >
+            <img
+              src={reactLogo}
+              className='h-[6em] p-[1.5em] transition-[filter] duration-[750ms] will-change-[filter] hover:drop-shadow-[0_0_2em_#61dafb]'
+              alt='React logo'
+            />
+          </a>
+        </div>
+        <p className='text-balance'>
+          Click on the Tauri, Vite, and React logos to learn more.
+        </p>
+
+        <form
+          className='flex w-full flex-col items-stretch justify-center gap-3 sm:w-auto sm:flex-row sm:items-center'
+          onSubmit={(e) => {
+            e.preventDefault();
+            greet();
+          }}
+        >
+          <input
+            id='greet-input'
+            className='w-full min-w-0 rounded-lg border border-transparent bg-white px-[1.2em] py-[0.6em] font-[inherit] text-[1em] font-medium text-[#0f0f0f] shadow-[0_2px_2px_rgba(0,0,0,0.2)] outline-none transition-[border-color] duration-[250ms] dark:bg-[#0f0f0f98] dark:text-white sm:w-[30rem]'
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder='Enter a name...'
+          />
+          <button
+            type='submit'
+            className='cursor-pointer rounded-lg border border-transparent bg-white px-[1.6em] py-[0.6em] font-[inherit] text-[1em] font-medium text-[#0f0f0f] shadow-[0_2px_2px_rgba(0,0,0,0.2)] outline-none transition-[border-color] duration-[250ms] hover:border-[#396cd8] active:border-[#396cd8] active:bg-[#e8e8e8] dark:bg-[#0f0f0f98] dark:text-white dark:active:bg-[#0f0f0f69]'
+          >
+            Greet
+          </button>
+        </form>
+        <p className='min-h-8'>{greetMsg}</p>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className='row'
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id='greet-input'
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder='Enter a name...'
-        />
-        <button type='submit'>Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
